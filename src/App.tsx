09d3ff670f08db9334979a988dd3a8cd154b3c1b
svenpetermanec <sven.petermanec@gmail.com>
2022-06-27 22:42:45 +0200

    Add react router and define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path='/posts' element={<>Posts</>} />
+      <Route path='/post/:id' element={<>Single</>} />
+      <Route path='*' element={<Navigate to='/posts' />} />
+    </Routes>
+  );
 };
