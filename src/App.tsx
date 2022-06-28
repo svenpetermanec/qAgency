30b7042e107833c0ef014fb79853a283e037f79f
svenpetermanec <sven.petermanec@gmail.com>
2022-06-28 14:19:52 +0200

    Wrap routes in user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { createContext, useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import axios from './utils/axios';
+import { User } from './utils/interfaces/user.interface';
+
+export const UserContext = createContext<User[]>([]);
 
 export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get('/users');
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
-    <Routes>
-      <Route path='/posts' element={<>Posts</>} />
-      <Route path='/post/:id' element={<>Single</>} />
-      <Route path='*' element={<Navigate to='/posts' />} />
-    </Routes>
+    <UserContext.Provider value={users}>
+      <Routes>
+        <Route path='/posts' element={<>Posts</>} />
+        <Route path='/post/:id' element={<>Single</>} />
+        <Route path='*' element={<Navigate to='/posts' />} />
+      </Routes>
+    </UserContext.Provider>
   );
 };
